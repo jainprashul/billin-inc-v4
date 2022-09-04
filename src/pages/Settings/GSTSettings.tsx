f2592e1ff516ba59e6d7129b6fef79213ce5abbe
Prashul Jain <jainprashul@gmail.com>
2022-09-04 18:18:30 +0530

    Update GST settings

--- src/pages/Settings/GSTSettings.tsx
+++ src/pages/Settings/GSTSettings.tsx
@@ -1,26 +1,43 @@
 import { FormControlLabel, Switch } from '@mui/material'
 import React from 'react'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
-import { selectGstRateType, setGstRateInclusive } from '../../utils/utilsSlice'
+import { selectGstEnabled, selectGstRateType, setGstEnabled, setGstRateInclusive } from '../../utils/utilsSlice'
 
 type Props = {}
 
 const GSTSettings = (props: Props) => {
     const isInclusive = useAppSelector(selectGstRateType)
+    const gstEnabled = useAppSelector(selectGstEnabled)
     const dispatch = useAppDispatch();
     return (
-        <div>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+        }}>
             <FormControlLabel
-                label={isInclusive ? "Inclusive GST Rate" : "Exclusive GST Rate"}
+                label={gstEnabled ? "GST Enabled" : "GST Disabled"}
                 control={
                     <Switch
-                        checked={isInclusive}
+                        checked={gstEnabled}
                         onChange={(e, value) => {
-                            dispatch(setGstRateInclusive(value))
+                            dispatch(setGstEnabled(value))
                         }}
                     />
                 }
             />
+            {
+                gstEnabled && <FormControlLabel
+                    label={isInclusive ? "Inclusive GST Rate" : "Exclusive GST Rate"}
+                    control={
+                        <Switch
+                            checked={isInclusive}
+                            onChange={(e, value) => {
+                                dispatch(setGstRateInclusive(value))
+                            }}
+                        />
+                    }
+                />
+            }
         </div>
     )
 }
--- src/utils/utilsSlice.ts
+++ src/utils/utilsSlice.ts
@@ -2,16 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../app/store";
 
 interface utils {
-    gst : {
-        enabled : boolean;
-        inclusive : boolean; 
+    gst: {
+        enabled: boolean;
+        inclusive: boolean;
     }
 }
 
 const initialState: utils = {
-    gst : {
-        enabled : JSON.parse(localStorage.getItem("gstEnabled") || "false"),
-        inclusive : JSON.parse(localStorage.getItem("gstRateInclusive") || "false"),
+    gst: {
+        enabled: JSON.parse(localStorage.getItem("gstEnabled") || "false"),
+        inclusive: JSON.parse(localStorage.getItem("gstRateInclusive") || "false"),
     }
 };
 
@@ -20,11 +20,15 @@ export const utilsSlice = createSlice({
     initialState,
     name: "utils",
     reducers: {
-        setGstEnabled: (state, action : {payload : boolean} ) => {
+        setGstEnabled: (state, action: { payload: boolean }) => {
             state.gst.enabled = action.payload;
             localStorage.setItem("gstEnabled", JSON.stringify(action.payload));
+            if (action.payload === false) {
+                state.gst.inclusive = false;
+                localStorage.setItem("gstRateInclusive", JSON.stringify(false));
+            }
         },
-        setGstRateInclusive: (state, action : {payload : boolean} ) => {
+        setGstRateInclusive: (state, action: { payload: boolean }) => {
             state.gst.inclusive = action.payload;
             localStorage.setItem("gstRateInclusive", JSON.stringify(action.payload));
         },
