a4fe0c82c21e7401cd69cb9ee2cc6ba0ee878bf3
Prashul Jain <jainprashul@gmail.com>
2022-12-25 22:18:32 +0530

    Loafin TO Top

--- src/pages/Settings/Auth.tsx
+++ src/pages/Settings/Auth.tsx
@@ -6,19 +6,19 @@ import { useGoogle } from '../../utils/useGoogle'
 type Props = {}
 
 const Auth = (props: Props) => {
-  const {isSignedIn, user , signIn , signOut } = useGoogle();
+  const { isSignedIn, user, signIn, signOut } = useGoogle();
 
   return (
     <div>
-         {
+      {
         isSignedIn ? (<>
-            Google Account Sign in as :
-            <div style={{display:"flex", alignItems:"center", padding: 4, margin: 2}}>
-            <Avatar src={user?.getImageUrl()} /> 
-                <span style={{marginLeft:"10px"}}>{user?.getName()}</span>
-            </div>
+          Google Account Sign in as :
+          <div style={{ display: "flex", alignItems: "center", padding: 4, margin: 2 }}>
+            <Avatar src={user?.getImageUrl()} />
+            <span style={{ marginLeft: "10px" }}>{user?.getName()}</span>
+          </div>
           <Button onClick={signOut} startIcon={<Logout />}>Sign Out</Button>
-          </>
+        </>
         ) : (
           <Button onClick={signIn} startIcon={<Login />}>Sign In</Button>
         )
