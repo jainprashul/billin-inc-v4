ea32cbb9d9664bb823c759c6be544b2571d8c8c0
Prashul Jain <jainprashul@gmail.com>
2023-01-06 02:26:16 +0530

    Remove unused ones

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material'
+import {  Button, Typography } from '@mui/material'
 import React from 'react'
 import { checkLicense } from '../../services/lisensing'
 import Lottie from 'lottie-react';
